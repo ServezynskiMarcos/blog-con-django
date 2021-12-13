bd22ddc3f095496fd86b6174f97d7c531fd667c2
Servezynski <servezynskimarcos1@gmail.com>
2021-12-13 10:19:19 -0300

    act 2

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProyectoWebApp',
-    'registro',
+    'registroApp',
 ]
 
 MIDDLEWARE = [
--- ProyectoWeb/ProyectoWeb/urls.py
+++ ProyectoWeb/ProyectoWeb/urls.py
@@ -7,5 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("ProyectoWebApp.urls")),
-    path('',include("registro.urls")),
+    path('',include("registroApp.urls")),
 ]
